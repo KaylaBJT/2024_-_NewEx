108e2b8244a2c8ddab08b7ed800458f0e7db2971
Lizaveta <drago719@mail.ru>
2024-11-28 15:01:36 +0300

    Ex010_MethodArray project

---
+++ Ex010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length; // возвращает длину - кол-во элементов массива
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break; //прерывает цикл, если найдено первое нужное значение
+    }
+    // index = index + 1
+    index++;
+}
