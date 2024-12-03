53d73c8fdab343ef95102555c9940baabbe07ed1
Lizaveta <drago719@mail.ru>
2024-12-03 13:54:40 +0300

    Ex012_Methods

--- Ex012_Methods/Program.cs
+++ Ex012_Methods/Program.cs
@@ -32,15 +32,27 @@ int year = Method3();
 //Console.WriteLine(year);
 
 //Вид 4. Методы, которые что-то принимают и что-то возвращают
+//string Method4(int count, string text)
+//{
+//int i = 0;
+//string result = String.Empty;
+//
+//while (i < count)
+//{
+//result = result + text;
+//i++;
+//    }
+//return result;
+//}
+//string res = Method4(10, "я");
+//Console.WriteLine(res);
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-
-    while (i < count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
